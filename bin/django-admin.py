25d4b33556b85750d5cea0bd184e9459bed99c4c
velavok <sankovalev@gmail.com>
2019-05-01 20:56:26 +0300

    Remove ignored folders

--- bin/django-admin.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3.6
-
-import os; activate_this=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'activate_this.py'); exec(compile(open(activate_this).read(), activate_this, 'exec'), { '__file__': activate_this}); del os, activate_this
-
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
