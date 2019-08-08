240475f7913d48253c2c975fcd553c0d70937593
sankovalev <sankovalev@gmail.com>
2019-08-08 14:37:05 +0300

    Update model_resnet.py

--- Look4Face/backbone/model_resnet.py
+++ Look4Face/backbone/model_resnet.py
@@ -4,14 +4,12 @@ from torch.nn import Linear, Conv2d, BatchNorm1d, BatchNorm2d, ReLU, Dropout, Ma
 
 # Support: ['ResNet_50', 'ResNet_101', 'ResNet_152']
 
-# свертка 3x3
 def conv3x3(in_planes, out_planes, stride = 1):
     """3x3 convolution with padding"""
 
     return Conv2d(in_planes, out_planes, kernel_size = 3, stride = stride,
                      padding = 1, bias = False)
 
-# свертка 1x1
 def conv1x1(in_planes, out_planes, stride = 1):
     """1x1 convolution"""
 
@@ -22,15 +20,15 @@ class BasicBlock(Module):
     '''
     Основной блок сетки (residual block)
     '''
-    expansion = 1 #расширение
+    expansion = 1
 
     def __init__(self, inplanes, planes, stride = 1, downsample = None):
         super(BasicBlock, self).__init__()
-        self.conv1 = conv3x3(inplanes, planes, stride)  #свертка 3x3
-        self.bn1 = BatchNorm2d(planes)                  #батч норм.
-        self.relu = ReLU(inplace = True)                #активация ReLU
-        self.conv2 = conv3x3(planes, planes)            #свертка 3x3
-        self.bn2 = BatchNorm2d(planes)                  #батч норм.
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = BatchNorm2d(planes)
+        self.relu = ReLU(inplace = True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = BatchNorm2d(planes)
         self.downsample = downsample
         self.stride = stride
 
@@ -40,7 +38,7 @@ class BasicBlock(Module):
         :param x: вход
         :return: выход
         '''
-        identity = x #начальный вход
+        identity = x
 
         out = self.conv1(x)   #свертка 3x3
         out = self.bn1(out)   #батч норм.
@@ -52,7 +50,7 @@ class BasicBlock(Module):
         if self.downsample is not None:
             identity = self.downsample(x)
 
-        out += identity       #прибавляем в выходу блока его вход. Должны быть одного размера!
+        out += identity
         out = self.relu(out)  #ReLU
 
         return out
@@ -82,7 +80,7 @@ class Bottleneck(Module):
         :param x: вход
         :return: выход
         '''
-        identity = x #начальный вход
+        identity = x
 
         out = self.conv1(x)     #свертка 1x1
         out = self.bn1(out)     #батч норм.
@@ -98,13 +96,12 @@ class Bottleneck(Module):
         if self.downsample is not None:
             identity = self.downsample(x)
 
-        out += identity         #прибавляем в выходу блока его вход. Должны быть одного размера!
+        out += identity         #прибавляем в выходу блока его вход.
         out = self.relu(out)    #активация ReLU
 
         return out
 
 
-#РАЗОБРАТЬ ЭТОТ КЛАСС
 class ResNet(Module):
     '''
     ResNet-сетка
@@ -113,12 +110,12 @@ class ResNet(Module):
     def __init__(self, input_size, block, layers, zero_init_residual = True):
         super(ResNet, self).__init__()
         assert input_size[0] in [112, 224], "input_size should be [112, 112] or [224, 224]"
-        self.inplanes = 64  #входное число слоев
-        self.conv1 = Conv2d(3, 64, kernel_size = 7, stride = 2, padding = 3, bias = False) #сверточный слой
-        self.bn1 = BatchNorm2d(64) #батч норм.
-        self.relu = ReLU(inplace = True) #активация ReLU
-        self.maxpool = MaxPool2d(kernel_size = 3, stride = 2, padding = 1) #макс пулинг 3x3
-        self.layer1 = self._make_layer(block, 64, layers[0]) #
+        self.inplanes = 64
+        self.conv1 = Conv2d(3, 64, kernel_size = 7, stride = 2, padding = 3, bias = False)
+        self.bn1 = BatchNorm2d(64)
+        self.relu = ReLU(inplace = True)
+        self.maxpool = MaxPool2d(kernel_size = 3, stride = 2, padding = 1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
         self.layer2 = self._make_layer(block, 128, layers[1], stride = 2)
         self.layer3 = self._make_layer(block, 256, layers[2], stride = 2)
         self.layer4 = self._make_layer(block, 512, layers[3], stride = 2)
